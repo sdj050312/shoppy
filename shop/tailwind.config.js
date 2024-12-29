a0e24419099081ce848b607c7e1edbe391142124
sdj050312 <sdj0503123@gmail.com>
2024-12-29 20:42:38 +0900

    Delete shop directory

--- shop/tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Covers all common React file extensions
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
